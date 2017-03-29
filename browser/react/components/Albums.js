f099f0a9836152755b7c76be2b868c883e0030bf
Allison Alexander <aveleber@hotmailcom>
2017-03-29 11:03:13 -0400

    selected album is working!

--- browser/react/components/Albums.js
+++ browser/react/components/Albums.js
@@ -4,26 +4,27 @@ import { Link } from 'react-router'
 const Albums = (props) => {
 
   const albums = props.albums;
-  const selectAlbum = props.selectAlbum;
+  //const selectAlbum = props.selectAlbum;
+  console.log('props', props);
 
   return (
     <div>
       <h3>Albums</h3>
       <div className="row">
-      {
+{
         albums.map(album => (
-          <div className="col-xs-4" key={ album.id }>
-            <Link to={`/albums/${this.props.params.albumId}`}>Go to an Album</Link>
-              <img src={ album.imageUrl } />
-              <div className="caption">
-                <h5>
-                  <span>{ album.name }</span>
-                </h5>
-                <small>{ album.songs.length } songs</small>
-              </div>
-
+          <div className="col-xs-4 album" key={ album.id }>
+            <Link to={`/albums/${album.id}`}>
+                <img src={ album.imageUrl } />
+                <div className="caption">
+                  <h5>
+                    <span>{ album.name }</span>
+                  </h5>
+                  <small>{ album.songs.length } songs</small>
+                </div>
+            </Link>
           </div>
-        ), this)
+        ))
       }
       </div>
     </div>
@@ -36,3 +37,21 @@ export default Albums;
 // <Link to={`/albums/${this.props.albumId}`}>Go to an Album</Link>
 // <a className="thumbnail" href="#" onClick={() => selectAlbum(album.id)}>
 //   </a>
+
+//<Link to={`/albums/${props.params.albumId}`}>Go to an Album</Link>
+
+// {
+//         albums.map(album => (
+//           <div className="col-xs-4 album" key={ album.id }>
+//             <Link to={`/albums/${props.params.albumId}`}>Go to an Album</Link>
+//               <img src={ album.imageUrl } />
+//               <div className="caption">
+//                 <h5>
+//                   <span>{ album.name }</span>
+//                 </h5>
+//                 <small>{ album.songs.length } songs</small>
+//               </div>
+
+//           </div>
+//         ))
+//       }
